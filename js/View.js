a8ecae063b6bccd8522bd90982dc891d3093922e
shuchenliu <shuchenliu2014@gmail.com>
2017-11-11 18:55:52 -0800

    FIX: Map click will now hide sidebar

--- js/View.js
+++ js/View.js
@@ -13,9 +13,17 @@ const filterButton = document.getElementById('filter');
 const sidebar = document.getElementsByClassName('sidebar')[0];
 const closeButton = document.getElementsByClassName('closeButton')[0]
 
+const mapHolder = document.getElementById('map');
+
 
 filterButton.addEventListener('click', hideSidebar);
 closeButton.addEventListener('click', hideSidebar);
+
+mapHolder.addEventListener('click', e => {
+  if (!sidebar.classList.contains('hide')) {
+    hideSidebar(e);
+  }
+});
 
 
 
